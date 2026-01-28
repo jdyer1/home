21c4e543a822e60bf9fd0f1734e85916ca2e4371
jdyer1 <jdyer@apache.org>
2026-01-28 16:29:51 -0600

    menu improvement

--- src/app/Menu.tsx
+++ src/app/Menu.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useRef } from 'react';
 import menuData from './menu-data.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library, IconName } from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
@@ -32,7 +32,7 @@ export const Menu: React.FC = () => {
 
   return (
     <nav
-      className="fixed left-0 top-12 h-[calc(100vh-6rem)] z-40 flex flex-col items-center bg-gray-50 text-gray-900 py-4 font-cousine"
+      className="fixed left-0 top-0 h-full z-50 flex flex-col items-center bg-gray-900 text-gray-100 py-4 font-cousine"
       style={{ fontFamily: 'Cousine, monospace' }}
       aria-label="Main menu"
     >
@@ -44,13 +44,13 @@ export const Menu: React.FC = () => {
           onMouseLeave={handleMouseLeave}
         >
           <FontAwesomeIcon
-            icon={["fas", item.icon as IconName]}
-            className="transition-transform duration-200 text-2xl group-hover:scale-110 text-black"
+            icon={["fas", item.icon]}
+            className="transition-transform duration-200 text-2xl group-hover:scale-110"
             title={safeText(item.name)}
           />
           {expanded === idx && (
             <span
-              className="absolute left-10 top-1/2 -translate-y-1/2 bg-gray-50 text-black px-4 py-2 rounded shadow-lg whitespace-nowrap transition-opacity duration-200 opacity-100 border border-gray-300"
+              className="absolute left-10 top-1/2 -translate-y-1/2 bg-gray-800 text-gray-100 px-4 py-2 rounded shadow-lg whitespace-nowrap transition-opacity duration-200 opacity-100 border border-gray-700"
               style={{ fontFamily: 'Cousine, monospace' }}
             >
               {safeText(item.name)}
